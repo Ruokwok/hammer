17911055ff08c0c3c07d17d2be688e5453004861
Ruokwok <i@ruok.cc>
2024-01-18 21:50:50 +0800

    日志系统

--- src/main/java/cc/ruok/hammer/Logger.java
+++ src/main/java/cc/ruok/hammer/Logger.java
@@ -1,9 +1,23 @@
 package cc.ruok.hammer;
 
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import cn.hutool.log.StaticLog;
+
 public class Logger {
 
+    private static final Log log = LogFactory.get();
+
     public static void logException(Exception e) {
-        e.printStackTrace();
+        log.error(e);
+    }
+
+    public static void info(String str) {
+        log.info(str);
+    }
+
+    public static void warning(String str) {
+        log.warn(str);
     }
 
 }
