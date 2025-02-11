0d0556f323dc59e7325e11e2a6242c7980ab56fa
Ruokwok <i@ruok.cc>
2025-02-11 09:13:49 +0800

    安装模式

--- src/main/java/cc/ruok/hammer/Hammer.java
+++ src/main/java/cc/ruok/hammer/Hammer.java
@@ -3,17 +3,20 @@ package cc.ruok.hammer;
 import cc.ruok.hammer.engine.Engine;
 import cc.ruok.hammer.engine.api.EngineAPI;
 import cc.ruok.hammer.plugin.PluginManager;
+import cn.hutool.core.io.FileUtil;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.management.ManagementFactory;
 import java.nio.file.Files;
 
 public class Hammer {
 
     public static final File CONFIG_PATH = new File("config");
+    public static final File PROCESS_PATH = new File("process");
     public static HammerConfig config;
     public static final String VERSION = "1.0-SNAPSHOT";
 
@@ -38,6 +41,15 @@ public class Hammer {
         for (String param : args) {
             if (param.startsWith("--httpPort=")) config.httpPort = Integer.parseInt(param.substring(param.indexOf("=") + 1));
             if (param.startsWith("--httpsPort=")) config.httpsPort = Integer.parseInt(param.substring(param.indexOf("=") + 1));
+            if (param.equals("--installed=true") || param.equals("-install")) {
+                if (!PROCESS_PATH.exists()) {
+                    PROCESS_PATH.mkdir();
+                }
+                long pid = ManagementFactory.getRuntimeMXBean().getPid();
+                FileUtil.writeUtf8String(String.valueOf(pid), PROCESS_PATH + "/PID");
+                FileUtil.writeUtf8String(String.valueOf(config.httpPort), PROCESS_PATH + "/HTTP");
+                FileUtil.writeUtf8String(String.valueOf(config.httpsPort), PROCESS_PATH + "/HTTPS");
+            }
         }
         if (!CONFIG_PATH.exists()) {
             CONFIG_PATH.mkdir();
